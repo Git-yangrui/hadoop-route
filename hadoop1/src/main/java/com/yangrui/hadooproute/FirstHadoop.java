2cf96bfa689920cbaec7156290fa58af5c30c6d9
yangrui <645374132@qq.com>
2017-07-20 09:45:42 +0800

    sdsd

--- hadoop1/src/main/java/com/yangrui/hadooproute/FirstHadoop.java
+++ hadoop1/src/main/java/com/yangrui/hadooproute/FirstHadoop.java
@@ -1,4 +1,10 @@
 package com.yangrui.hadooproute;
 
+import org.apache.hadoop.hive.ql.exec.UDF;
+
 public class FirstHadoop {
+    public static void main(String[] args) {
+        String s=new String("sssss");
+  System.out.print(s);
+    }
 }
