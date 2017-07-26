71daf236bd3f84faf3a68430cdf6f3f06d3da8cc
yangrui <645374132@qq.com>
2017-07-27 00:30:12 +0800

    1233

---
+++ zookeeper/src/main/java/com/yangrui/hbase/HBaseOperation.java
@@ -0,0 +1,33 @@
+package com.yangrui.hbase;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.Cell;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.client.Get;
+import org.apache.hadoop.hbase.client.HBaseAdmin;
+import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.util.Bytes;
+
+import java.io.IOException;
+
+
+public class HBaseOperation {
+
+    public static void main(String[] args) throws IOException {
+        String usertable="user";
+        HTable hBaseTable = getHBaseTable(usertable);
+        Get get=new Get(Bytes.toBytes("100001"));
+        Result result = hBaseTable.get(get);
+        for (Cell cells:result.rawCells()) {
+
+        }
+
+
+    }
+
+    private static HTable getHBaseTable(String usertable) throws IOException {
+        Configuration configuration = HBaseConfiguration.create();
+        return new HTable(configuration,usertable);
+    }
+}
