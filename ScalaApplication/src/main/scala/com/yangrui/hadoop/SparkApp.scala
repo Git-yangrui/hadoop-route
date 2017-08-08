01020ed9644657460104ad89f3e184c6709845e2
yangrui <645374132@qq.com>
2017-08-09 01:53:49 +0800

    commit

--- ScalaApplication/src/main/scala/com/yangrui/hadoop/SparkApp.scala
+++ ScalaApplication/src/main/scala/com/yangrui/hadoop/SparkApp.scala
@@ -5,7 +5,6 @@ import org.apache.spark.{SparkConf, SparkContext}
 object SparkApp {
   def main(args: Array[String]): Unit = {
 //    var sparkConf=new
-
     val sc = new SparkContext(new SparkConf().setAppName("Spark Count"))
     val valueRDD=sc.textFile("").flatMap(_.split("")).map((_,1))
 
